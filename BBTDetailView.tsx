3415ae038eed13b788f898c0cc4740c352cb9c01
Vishal S B <79051939+Freakt01d@users.noreply.github.com>
2025-06-26 06:25:02 +0530

    Update BBTDetailView.tsx

--- BBTDetailView.tsx
+++ BBTDetailView.tsx
@@ -1,11 +1,5 @@
-internal static async Task<List<string>> GetAsJsonRolesAsync(this HttpClient client, Uri relativeUri, CancellationToken token)
-{
-    var response = await client.GetAsync(relativeUri, token);
-    response.EnsureSuccessStatusCode();
-
-    var content = await response.Content.ReadAsStringAsync();
-    return JArray.Parse(content)
-                 .Select(role => role?.ToString())
-                 .Where(role => !string.IsNullOrEmpty(role))
-                 .ToList();
-}
+var content = await response.Content.ReadAsStringAsync();
+return JArray.Parse(content)
+             .Select(role => role?["role"]?.ToString())
+             .Where(role => !string.IsNullOrEmpty(role))
+             .ToList();
