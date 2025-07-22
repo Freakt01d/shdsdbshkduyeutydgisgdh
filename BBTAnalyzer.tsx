eae46003b7a758cb3f922b121139cd65145cfa1e
Vishal S B <79051939+Freakt01d@users.noreply.github.com>
2025-07-22 10:06:51 +0530

    Update BBTAnalyzer.tsx

--- BBTAnalyzer.tsx
+++ BBTAnalyzer.tsx
@@ -5,7 +5,7 @@ rec003,user002,clothing,59.90,2025-07-22T08:45:00Z
 
 aws s3 cp .\data.csv s3://my-file-uploads-bucket/uploads/user001/data.csv --region ap-southeast-2
 
-aws dynamodb put-item --table-name FileProcessingJobs --item '{ "user_id": {"S": "user001"}, "job_id": {"S": "job-001"}, "status": {"S": "pending"}, "filename": {"S": "data.csv"}, "s3_path": {"S": "s3://my-file-uploads-bucket/uploads/user001/data.csv"}, "created_at": {"S": "2025-07-22T10:00:00Z"} }' --region ap-southeast-2
+aws dynamodb put-item --table-name FileProcessingJobs --item '{\"user_id\": {\"S\": \"user001\"}, \"job_id\": {\"S\": \"job-001\"}, \"status\": {\"S\": \"pending\"}, \"filename\": {\"S\": \"data.csv\"}, \"s3_path\": {\"S\": \"s3://my-file-uploads-bucket/uploads/user001/data.csv\"}, \"created_at\": {\"S\": \"2025-07-22T10:00:00Z\"}}' --region ap-southeast-2
 
 aws dynamodb delete-item --table-name FileProcessingJobs --key "{\"job_id\": {\"S\": \"job-001\"}, \"user_id\": {\"S\": \"user001\"}}" --region ap-southeast-2
 
