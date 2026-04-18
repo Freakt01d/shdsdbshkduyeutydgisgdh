906d2117a5d330ef76fea86569df4e0bfafc860a
Vishal S B <79051939+Freakt01d@users.noreply.github.com>
2026-04-18 14:07:08 +0530

    Update BBTDetailView.tsx

--- BBTDetailView.tsx
+++ BBTDetailView.tsx
@@ -1,28 +1,11 @@
-using (var conn = new OracleConnection(_reddDbConnStr))
-{
-    conn.Open();
+SELECT indexname, tablename 
+FROM pg_indexes 
+WHERE schemaname = 'redservice' 
+AND tablename LIKE 't_raw_detail_audit%' 
+LIMIT 20;
 
-    if (toInsert.Any())
-    {
-        using (var cmd = conn.CreateCommand())
-        {
-            cmd.CommandText = "INSERT INTO TraceFiles (cusip) VALUES (:cusip)";
-            cmd.ArrayBindCount = toInsert.Count;
-            cmd.Parameters.Add("cusip", OracleDbType.Varchar2, toInsert.ToArray(), ParameterDirection.Input);
-            cmd.ExecuteNonQuery();
-        }
-    }
-
-    if (toDelete.Any())
-    {
-        using (var cmd = conn.CreateCommand())
-        {
-            cmd.CommandText = "DELETE FROM TraceFiles WHERE cusipid = :cusip";
-            cmd.ArrayBindCount = toDelete.Count;
-            cmd.Parameters.Add("cusip", OracleDbType.Varchar2, toDelete.ToArray(), ParameterDirection.Input);
-            cmd.ExecuteNonQuery();
-        }
-    }
-
-    _logger.Info($"Inserted: {toInsert.Count}, Deleted: {toDelete.Count} for date {partitionDate:yyyy-MM-dd}");
-}
+SELECT indexname, tablename 
+FROM pg_indexes 
+WHERE schemaname = 'redservice' 
+AND tablename LIKE 't_raw_audit%' 
+LIMIT 20;
